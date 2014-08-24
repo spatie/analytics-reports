adaed9ad9a37f43cb971f6bc7e30090f99f9710f
freek <freek@spatie.be>
2014-08-24 14:08:22 +0200

    commit

---
+++ src/config/analyticsReports.php
@@ -0,0 +1,28 @@
+<?php return
+
+    [
+        /*
+        |--------------------------------------------------------------------------
+        | Analytics reports site id
+        |--------------------------------------------------------------------------
+        |
+        | This site id is used to retrieve and display Google Analytics statistics
+        | in the admin-section. Should be something like ga:xxxxxxxx.
+        |
+        */
+
+        'siteId' => '',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Cache Lifetime
+        |--------------------------------------------------------------------------
+        |
+        | The amount of minutes the Google API responses will be cached.
+        | If you set this to zero, the responses won't be cached at all.
+        |
+        */
+
+        'cacheLifetime' => 60 * 24 * 2
+
+    ];
